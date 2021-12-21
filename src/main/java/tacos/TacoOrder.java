4a59eb5cd45a0a799d5d0ec0f02f5a4e8f168818
Bogdan Creanga <bogdan.creanga@serinf.it>
2021-12-21 12:49:57 +0100

    add persistence
    add dependencies, create Repository for Ingredient
    feed database with ingredients' data
    
    Notice that it works without @Autorwired in constructors

--- src/main/java/tacos/TacoOrder.java
+++ src/main/java/tacos/TacoOrder.java
@@ -7,11 +7,20 @@ import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.CreditCardNumber;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Date;
+
 import lombok.Data;
 
 @Data
-public class TacoOrder {
+public class TacoOrder implements Serializable {
+	
+	private static final long serialVersionID = 1L;
+	
+	private Long id;
+		
+	private Date placedAt;
 
 	@NotBlank(message="Delivery name is required")
 	private String deliveryName;
--- src/main/java/tacos/web/DesignTacoController.java
+++ src/main/java/tacos/web/DesignTacoController.java
@@ -1,8 +1,7 @@
 package tacos.web;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import javax.validation.Valid;
 
@@ -19,27 +18,24 @@ import lombok.extern.slf4j.Slf4j;
 import tacos.Ingredient;
 import tacos.Ingredient.Type;
 import tacos.Taco;
+import tacos.data.IngredientRepository;
 
 @Slf4j
 @Controller
 @RequestMapping("/design")
 @SessionAttributes("tacoOrder")
 public class DesignTacoController {
+	
+	private IngredientRepository ingredientRepository;
+	
+	//@Autowired
+	public DesignTacoController(IngredientRepository ingredientRepository) {
+		this.ingredientRepository = ingredientRepository;
+	}
 
 	@ModelAttribute
 	public void addIngredientsToModel(Model model) {
-		List<Ingredient> ingredients = Arrays.asList(
-				new Ingredient("FLTO", "Flour Tortilla", Type.WRAP),
-				new Ingredient("COTO", "Corn Tortilla", Type.WRAP),
-				new Ingredient("GRBF", "Ground Beef", Type.PROTEIN),
-				new Ingredient("CARN", "Carnitas", Type.PROTEIN),
-				new Ingredient("TMTO", "Diced Tomatoes", Type.VEGGIES),
-				new Ingredient("LETC", "Lettuce", Type.VEGGIES),
-				new Ingredient("CHED", "Cheddar", Type.CHEESE),
-				new Ingredient("JACK", "Monterrey Jack", Type.CHEESE),
-				new Ingredient("SLSA", "Salsa", Type.SAUCE),
-				new Ingredient("SRCR", "Sour Cream", Type.SAUCE));
-
+		Iterable<Ingredient> ingredients = ingredientRepository.findAll();
 		Type[] types = Ingredient.Type.values();
 		for (Type type : types) {
 			model.addAttribute(type.toString().toLowerCase(), filterByType(ingredients, type));
@@ -52,8 +48,12 @@ public class DesignTacoController {
 		return "design";
 	}
 
-	private Iterable<Ingredient> filterByType(List<Ingredient> ingredients, Type type) {
-		return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
+	private Iterable<Ingredient> filterByType(Iterable<Ingredient> ingredients, Type type) {
+		//return ingredients.stream().filter(x -> x.getType().equals(type)).collect(Collectors.toList());
+		return StreamSupport.stream(ingredients.spliterator(), false)
+			.filter(x -> x.getType().equals(type))
+			.collect(Collectors.toList());
+		
 	}
 	
 	@PostMapping
